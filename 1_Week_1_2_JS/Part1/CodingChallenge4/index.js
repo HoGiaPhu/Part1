83e0773e8f2ad880739082885f4a3b137d45042d
H82M19\Student <Student>
2025-01-08 08:48:28 +0700

    tuan 1 part 1

--- 1_Week_1_2_JS/Part1/CodingChallenge4/index.js
+++ 1_Week_1_2_JS/Part1/CodingChallenge4/index.js
@@ -1,1 +1,29 @@
 var tip;
+var bill = 275
+if (bill > 50 && bill < 300) {
+    tip = bill * 0.15;
+    console.log("The bill are " + bill + " the tip are " + tip + " and total are: " + (bill + tip));
+} else{
+    tip = bill * 0.2;
+    console.log("The bill are " + bill + " the tip are " + tip + " and total are: " + (bill + tip));
+}
+
+
+var bill = 40
+if (bill > 50 && bill < 300) {
+    tip = bill * 0.15;
+    console.log("The bill are " + bill + " the tip are " + tip + " and total are: " + (bill + tip));
+} else{
+    tip = bill * 0.2;
+    console.log("The bill are " + bill + " the tip are " + tip + " and total are: " + (bill + tip));
+}
+
+
+var bill = 430
+if (bill > 50 && bill < 300) {
+    tip = bill * 0.15;
+    console.log("The bill are " + bill + " the tip are " + tip + " and total are: " + (bill + tip));
+} else{
+    tip = bill * 0.2;
+    console.log("The bill are " + bill + " the tip are " + tip + " and total are: " + (bill + tip));
+}
