82b8bc77097a9c7c22f647b4c33818aff885b7ea
HoGiaPhu <phutk999@gmail.com>
2025-04-15 23:14:42 +0000

    Tuan5

--- Tuan5/Tuan5/src/App.jsx
+++ Tuan5/Tuan5/src/App.jsx
@@ -2,18 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
-import Footer from './components/footer'
-import Content from './components/content'
 
 function App() {
-  const [count, setCount] = useState(0)
+  var style ={
+    li:{
+      listStyleType: 'none',
+      margin: '10px',
+      paddingLeft: '10px',
+      border: '1px solid #ccc',
+      borderRadius: '5px',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }
+  }
+
+  const [todos, setToDos] = useState([])
+  const [text, setText] = useState("")
+
+  function handleChange(event) {
+    setText(event.target.value)
+  }
+
+  function handleClick() {
+    setToDos([...todos,text])
+  }
 
   return (
     <>
-      <Header></Header>
-      <Content></Content>
-      <Footer></Footer>
+      <input onChange={handleChange} type="text" />
+      <br />
+      <br />
+      <button onClick={handleClick}>Add</button>
+
+      <ul>
+        {todos.map((todo,i) => (
+          <li style={style.li} key={i}> {todo}
+
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
